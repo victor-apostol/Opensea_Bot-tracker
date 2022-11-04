a7fd9607964c156f7af437ad9227bd5e7f014a38
Future of France <98639525+FutureFrance@users.noreply.github.com>
2022-11-04 21:57:04 +0200

    Update Track_opensea_wallet.py
    Switched to use the chromnium driver and updated the HTML elements i am searching for

--- Track_opensea_wallet.py
+++ Track_opensea_wallet.py
@@ -1,8 +1,9 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import chromedriver_binary
 import random
 import os
 import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 # Function To efficiently search throw html document, to use lest repetitive code
 def search(tag, clasa, cont):
@@ -15,10 +16,9 @@ def execute(wallet_owner):
     with open("opensea_html.txt", "r+") as f:
         previous_action = f.read()
 
-    os.environ["PATH"] = r"C:/browsers_drivers/geckodriver-v0.30.0-win64"
-
     # Instantiating a web browser session to visit Opensea website and scroll down the page to get loaded data
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome()
+
     driver.maximize_window()
     url = f"https://opensea.io/{wallet_owner}?tab=activity&search[chains][0]=ETHEREUM&search[eventTypes][0]=AUCTION_SUCCESSFUL&search[eventTypes][1]=AUCTION_CREATED"
     driver.get(url)
@@ -33,18 +33,18 @@ def execute(wallet_owner):
 
     # Searching for the list/sale action, collection name, "nft id" and the NFT price in the html code from Opensea
     try:
-        action_type = search('h6', 'Blockreact__Block-sc-1xf18x6-0 Textreact__Text-sc-1w94ul3-0 ehCsVi ibqWjk', content).text
-        collection_name = search('a', 'styles__StyledLink-sc-l6elh8-0 ekTmzq CollectionLink--link CollectionLink--isSmall', content).text
-        nft_id = search('div', 'Overflowreact__OverflowContainer-sc-7qr9y8-0 jRbcys', content).text
-        asset_price = search('div', 'Overflowreact__OverflowContainer-sc-7qr9y8-0 jPSCbX Price--amount', content).text
-        nft_link = search('a', 'styles__StyledLink-sc-l6elh8-0 ekTmzq styles__CoverLink-sc-nz4knd-1 givILt', content)['href']
-        nft_image = search('div', 'Imagereact__DivContainer-sc-dy59cl-0 kMPTZo Image--fade Image--isImageLoaded Image--isImageLoaderVisible AssetMedia--img', content).img['src']
+        action_type = search('h6', 'sc-29427738-0 sc-bdnxRM figDpC iPAlIP', content).text
+        collection_name = search('span', 'sc-29427738-0 dVNeWL', content).text
+        nft_id = search('div', 'sc-6990c3a-0 ddgOiO', content).text
+        asset_price = search('div', 'sc-6990c3a-0 kezsvr Price--amount', content).text
+        nft_link = search('a', 'sc-1f719d57-0 fKAlPV CollectionLink--link CollectionLink--isSmall', content)['href']
+        nft_image = search('div', 'sc-f087f95e-0 sc-f087f95e-1 gwpnfr gyivza AssetMedia--img', content).img['src']
         nft_id = nft_id[:len(nft_id) - 1]
         asset_price = asset_price[:len(asset_price) - 1]
 
         # Structured in a string all those 4 pieces of data we were able to retrieve in compare_transform variable
         compare_transform = f'{action_type}|{collection_name}|{nft_id}|{asset_price}'
-        discord_message = f'**{action_type.upper()}**: {collection_name}, {nft_id}, for the price of: {asset_price} ETH'
+        discord_message = f'**{action_type.upper()}**: {collection_name}, {nft_id}, for the price of: {asset_price[:len(asset_price)-3]} ETH'
     except:
         return "Data corrupted"
 
